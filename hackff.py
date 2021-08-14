a9b45490b062e48cfaf3fdb41ea8184da546e8fc
Mr.SadClown <88814289+MrSadClown@users.noreply.github.com>
2021-08-14 20:01:37 +0700

    Add files via upload

---
+++ hackff.py
@@ -0,0 +1,32 @@
+#SCRIPT HACK AKUN FF FAKE
+#!/bin/python
+
+#module
+import os,sys,time
+#clear
+def hapus():
+       os.system('clear')
+
+#tampilan
+def menu():
+     hapus()
+     os.system('figlet Hack akun FF') 
+     print ('{•}============================={•}')
+     print ('{√}Author    : Mr. SadClown     {√}')
+     print ('{√}Team      : WHITE CYBER UNION{√}')
+     print ('{•}============================={•}')
+     id = input ('Masukan ID Target :')
+     time.sleep(1)
+     print('Sabar Tod Lagi Proses Pembobolan')
+     time.sleep(5)
+     print('      ')
+     pas = input('Masukan Pasword baru yg mudah di ingat : ')
+     time.sleep(3)
+     print('      ')
+     print('================================')
+     print(' Sucses Tod √√√√')
+     print('    ')
+     print(' ID Target : ' +  id) 
+     print(' Pasword. : ' + pas)
+
+menu()
